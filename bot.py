bfa1310fa80fcc34f91e2b23c50be25191dda3bb
yuanyinOuO <yuanyinOuO@gmail.com>
2022-05-31 15:59:13 +0800

    5.31/3.58

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 from msilib.schema import File
 import discord
 from discord.ext import commands
+import json
+import random
 
 intents = discord.Intents.default()
 intents.members = True
+
+with open('setting.json' , mode='r' , encoding='utf8') as jfile:
+    jdate = json.load(jfile)
 
 bot = commands.Bot(command_prefix=',' , intents = intents)
 
@@ -13,24 +18,30 @@ async def on_ready():
 
 @bot.event
 async def on_member_join(member):
-    channel = bot.get_channel(981018005817421904)
-    await channel.send(f'{member} 他進來了!')
+    channel = bot.get_channel(int(jdate['joinchannel']))
+    await channel.send(f'恭迎{member}降臨 渡世靈顯四方 ')
 
 @bot.event
 async def on_member_remove(member):
-    channel = bot.get_channel(981018774478151720)
-    await channel.send(f'{member} 他離開了!')
+    channel = bot.get_channel(int(jdate['removechannel']))
+    await channel.send(f'{member} 默默的離開伺服器QQ')
 
 @bot.command()
 async def ping(ctx):
     await ctx.send(f'{round(bot.latency*1000)}(ms)')
     
 @bot.command()
-async def pic(ctx):
-    pic = discord.File('C:\\Users\\ACER\\OneDrive\\文件\\GitHub\\yuanyin-s-bot\\pic\\miku.jpg')
-    await ctx.send(file= pic)
+async def hi(ctx):
+    hi = discord.File(jdate['hibot'])
+    await ctx.send(file= hi)
 
-bot.run("OTgxMDI3MzYzMDM2OTg3NDYy.Gcr6Vu.UHJ528RCCxmPPwNiyGGYgp4vMKSrYo3JVsofsM")
+@bot.command()
+async def give(ctx):
+    random_pic = random.choice(jdate['give'])
+    give = discord.File(random_pic)
+    await ctx.send(file= give)
+
+bot.run(jdate['TOKEN'])
 
 
 
